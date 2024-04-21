c14a5907cf0944bc89d5828b36d3342013088579
kritika <103331480+kri4@users.noreply.github.com>
2024-04-20 18:42:58 -0700

    updating quizzes mongodb

---
+++ Quizzes/schema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const quizSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    course: String,
+    title: String,
+    description: String,
+    quizType: {
+        type: String,
+        enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"],
+    },
+    points: Number,
+    assignmentGroup: {
+        type: String,
+        enum: ["Quizzes", "Exams", "Assignments", "Project"],
+    },
+    shuffleAnswers: Boolean,
+    timeLimit: Number, // in minutes
+    multipleAttempts: Boolean,
+    showCorrectAnswers: Boolean,
+    accessCode: String,
+    oneQuestionAtATime: Boolean,
+    webcamRequired: Boolean,
+    lockQuestionsAfterAnswering: Boolean,
+    dueDate: Date,
+    availableDate: Date,
+    untilDate: Date,
+},   { collection: "users" });
+export default quizSchema;
