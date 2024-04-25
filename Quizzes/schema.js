48a0a003c7f6a77292be3e83ea35fb3fda767951
LalCelik <114354209+LalCelik@users.noreply.github.com>
2024-04-24 21:30:59 -0400

    h

--- Quizzes/schema.js
+++ Quizzes/schema.js
@@ -2,18 +2,18 @@ import mongoose from "mongoose";
 
 const quizSchema = new mongoose.Schema({
     published: Boolean,
-    name: String,
+    name: String, 
     course: String,
     title: String,
     description: String,
     quizType: {
         type: String,
-        enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"],
+        enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"], default: "Graded Quiz"
     },
     points: Number,
     assignmentGroup: {
         type: String,
-        enum: ["Quizzes", "Exams", "Assignments", "Project"],
+        enum: ["Quizzes", "Exams", "Assignments", "Project"], default: "Quizzes"
     },
     shuffleAnswers: Boolean,
     timeLimit: Number, // in minutes
